2cd8b85de6971ef8aeacd37067a4f496ddd19626
fernando.velazquez <c-fernando.velazquez@khoros.com>
2020-05-23 01:46:10 -0300

    Test React app build

---
+++ precache-manifest.75ac52cb07b4756ff1441a822340df6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9ea694e9122a48c11d287bf5c3ea4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2d7a159f12aea69c424",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33a92c48424489a1deb1",
+    "url": "/static/js/2.19a4b4ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19a4b4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d7a159f12aea69c424",
+    "url": "/static/js/main.e87fe0bf.chunk.js"
+  },
+  {
+    "revision": "5ea5abf03b2fbcfe55e0",
+    "url": "/static/js/runtime-main.ed647954.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
